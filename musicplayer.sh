a4b61f464c6a81f5f0eef71a84168f8b5f168997
kilian888 <konstantinhae@gmail.com>
2021-03-20 22:19:29 +0100

    ww

--- musicplayer.sh
+++ musicplayer.sh
@@ -1,56 +1,87 @@
 #!/bin/bash
 
-STATEFILE="/home/pi/state"
+STATEFILE="state.txt"
+
+settozero(){
+    indexline=$(pacmd list-sink-inputs | egrep -B 21 "$pidtokill" | egrep "index")
+    length=${#indexline}
+    let l=length-11
+    index=${indexline:11:$l}
+    pacmd set-sink-input-volume "$index" "1"
+}
+
 
 fadeout(){
-    count=65535
-    for i in {1..50}
+    count=32767
+    for i in {1..40} 
     do
         indexline=$(pacmd list-sink-inputs | egrep -B 21 "$pidtokill" | egrep "index")
         length=${#indexline}
-        echo "$indexline"
         let l=length-11
         index=${indexline:11:$l}
-        echo "$index"
-        len=${#index}
-        echo "$len"
         pacmd set-sink-input-volume "$index" "$count"
-        let count=count-1000
+        let count=count-760
         sleep .1
     done
 }
 
 fadein(){
-    for i in {1..90}
+    
+    count=1
+    for i in {1..40} 
     do
-        amixer -q sset Master 3%+
+        indexline=$(pacmd list-sink-inputs | egrep -B 21 "$pidtokill" | egrep "index")
+        length=${#indexline}
+        let l=length-11
+        index=${indexline:11:$l}
+        pacmd set-sink-input-volume "$index" $count
+        let count=count+819
         sleep .1
     done
 }
-amixer -q sset Master 100%
-Pathtofile="~/hellomercedes/state.txt"
+amixer -q sset Master 50%
+
 statevariable="base"
 pidtokill=0
-aplay ~/hellomercedes/file_example_WAV_1MG.wav &
+
 #precondition: volume is up at working vol
 while true; do
-
     if  grep -q "AMG" $STATEFILE && ! [ "$statevariable" == "AMG" ]; then
         fadeout
         kill -9 "$pidtokill"
-        aplay ~/hellomercedes/file_example_WAV_1MG.wav &
+        aplay ~/musicplayer/AMG_Musik_def.wav &
         pidtokill="$!"
         statevariable="AMG"
-        fadein
+        indexline=$(pacmd list-sink-inputs | egrep -B 21 "$pidtokill" | egrep "index")
+        length=${#indexline}
+        let l=length-11
+        index=${indexline:11:$l}
+        echo "$index"
+        pacmd set-sink-input-volume "$index" "65500"
     fi
 
     if grep -q "EQC" $STATEFILE && ! [ "$statevariable" == "EQC" ]; then
         fadeout
         kill -9 "$pidtokill"
-        aplay ~/hellomercedes/organfinale.wav &
-        pidtokill="$!"
+        
         statevariable="EQC"
+        amixer -q sset Master 0%
+        aplay ~/musicplayer/'EQC-Sound2-wind - 20.03.21, 12.wav' &
+        pidtokill="$!"
+        settozero
+        amixer -q sset Master 100%
         fadein
     fi
 
+    if grep -q "LOUNGE" $STATEFILE && ! [ "$statevariable" == "LOUNGE" ]; then
+        
+        statevariable="LOUNGE"
+        amixer -q sset Master 0%
+        aplay ~/musicplayer/Lounge_Musik_def.wav &
+        pidtokill="$!"
+        settozero
+        amixer -q sset Master 100%
+        fadein
+    fi
+    sleep .1
 done
